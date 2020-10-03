7d16b34b8353f330edc8cdd666940fbf25ec23b4
MelnikovAleksei <melnikov.aleksei.alexeyevich@gmail.com>
2020-10-03 19:26:31 +0300

    feat: rename class, add methods

--- components/Popup.js
+++ components/Popup.js
@@ -1,8 +1,26 @@
-export class Popup {
-  constructor(popupSelector) {
+export class PopupWithAddForm {
+  constructor(popupSelector, { submit }) {
     this._popupSelector = popupSelector;
+    this._submit = submit;
     this._popupElement = document.querySelector(this._popupSelector);
+    this._formElement = this._popupElement.querySelector('.popup__form');
+    this._handleSubmit = this._handleSubmit.bind(this);
     this._handleClick = this._handleClick.bind(this);
+  }
+
+  _getInputValue () {
+    this._inputsList = Array.from(this._popupElement.querySelectorAll('.popup__input'));
+    const inputsData = {};
+    this._inputsList.forEach(input => {
+      inputsData[input.name] = input.value;
+    })
+    return inputsData;
+  }
+
+  _handleSubmit (evt) {
+    evt.preventDefault();
+    this._submit(this._getInputValue())
+    this._close();
   }
 
   _handleClick (evt) {
@@ -12,10 +30,12 @@ export class Popup {
   }
 
   _removeEventListeners () {
+    this._formElement.removeEventListener('submit', this._handleSubmit);
     this._popupElement.removeEventListener('click', this._handleClick);
   }
 
   _setEventListeners () {
+    this._formElement.addEventListener('submit', this._handleSubmit);
     this._popupElement.addEventListener('click', this._handleClick);
   }
 
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
-import {Popup} from '../components/Popup.js';
+import {PopupWithAddForm} from '../components/Popup.js';
 
 import {addNoteButton} from '../utils/constants.js';
 
-const addNotePopup = new Popup('.popup-add-note');
+const addNotePopup = new PopupWithAddForm('.popup-add-note', {
+  submit: (data) => {
+    console.log(data)
+  }
+ });
 
 addNoteButton.addEventListener('click', () => {
   addNotePopup.open();
